01d9c352ac2f4ff14a708de059c804f72d2f1e14
EC2 Default User <ec2-user@ip-10-0-14-159.ec2.internal>
2022-08-30 03:16:08 +0000

    practicing code

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+# When performing a conditional use 'if' statement
+ 
+ 
+if 'a' < 'b':
+    print("CONDITION was true")  # Python automatically indents 4 spaces
+     
+if 'b' < 'a':
+    print("CONDITION was true")
+     
+if False:
+    print("Was true")
+else:
+    print("Was false")
+    
+if True:
+    print("Was true")
+else:
+    print("Was false")
